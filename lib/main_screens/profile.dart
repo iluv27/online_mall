9713f9a7896c896d78b981a41be8ae7348f9fa1c
iluv27 <ehiedujoy277@gmail.com>
2023-11-28 19:11:39 +0100

    new commit

--- lib/main_screens/profile.dart
+++ lib/main_screens/profile.dart
@@ -10,6 +10,32 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          elevation: 0,
+          expandedHeight: 140,
+          flexibleSpace: LayoutBuilder(builder: (context, constraints) {
+            return FlexibleSpaceBar(
+              title: AnimatedOpacity(
+                opacity: constraints.biggest.height < 120 ? 1 : 0,
+                duration: const Duration(milliseconds: 200),
+                child: const Text(
+                  'Account',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              background: Container(
+                decoration: const BoxDecoration(
+                  gradient:
+                      LinearGradient(colors: [Color(0xFF4D9CB2), Colors.brown]),
+                ),
+              ),
+            );
+          }),
+        )
+      ],
+    ));
   }
 }
