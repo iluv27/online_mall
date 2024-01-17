3ee9da5d76e7d29af58e9890e846f3e864af6f28
iluv27 <ehiedujoy277@gmail.com>
2024-01-17 06:22:48 +0100

    New commit

--- lib/auth/customer_signup.dart
+++ lib/auth/customer_signup.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/auth_widgets.dart';
+
 class CustomerRegister extends StatefulWidget {
   const CustomerRegister({super.key});
 
@@ -8,40 +10,160 @@ class CustomerRegister extends StatefulWidget {
 }
 
 class _CustomerRegisterState extends State<CustomerRegister> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool passwordVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        child: Center(
+          child: SingleChildScrollView(
+            keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+            reverse: true,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // THE SIGN UP TEXT
+                    const AuthHeaderLabel(headerLabel: 'Sign Up'),
+
+                    // THE CAMERA AND GALLERY INPUT
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20.0, horizontal: 40.0),
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundColor: Colors.purpleAccent,
+                          ),
+                        ),
+                        Column(
+                          children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.purple,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    topRight: Radius.circular(15)),
+                              ),
+                              child: IconButton(
+                                  onPressed: () {
+                                    debugPrint('Print image from camera');
+                                  },
+                                  icon: const Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.white,
+                                  )),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.purple,
+                                borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(15),
+                                    bottomRight: Radius.circular(15)),
+                              ),
+                              child: IconButton(
+                                  onPressed: () {
+                                    debugPrint('Print image from gallery');
+                                  },
+                                  icon: const Icon(
+                                    Icons.photo,
+                                    color: Colors.white,
+                                  )),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+
+                    // THE TEXT FIELDS
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your full name';
+                          }
+                          return null;
+                        },
+                        decoration: textFormDecoration.copyWith(),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your email';
+                          } else if (value.isEmailValid() == false) {
+                            return 'Invalid mail';
+                          } else if (value.isEmailValid() == true) {
+                            return null;
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: textFormDecoration.copyWith(
+                            labelText: 'Email Address',
+                            hintText: 'Enter your email'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                        obscureText: passwordVisible,
+                        decoration: textFormDecoration.copyWith(
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    passwordVisible = !passwordVisible;
+                                  });
+                                },
+                                icon: Icon(
+                                  passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Colors.purple,
+                                )),
+                            labelText: 'Password',
+                            hintText: 'Enter your password'),
+                      ),
+                    ),
+
+                    // ALREADY HAVE AN ACCOUNT TEXT
+                    HaveAccount(
+                        haveAccount: 'Already have an account?',
+                        actionLabel: 'Log in',
+                        onPressed: () {}),
+
+                    // SIGN UP BUTTON
+                    AuthMainButton(
+                        mainButtonLabel: 'Sign Up',
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            debugPrint('Valid');
+                          } else {
+                            debugPrint('Not valid');
+                          }
+                        })
+                  ],
                 ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/welcome_screen');
-                  },
-                  icon: const Icon(
-                    Icons.home_work,
-                    size: 40,
-                  ),
-                ),
-              ],
+              ),
             ),
-            const Row(
-              children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundColor: Colors.purpleAccent,
-                )
-              ],
-            )
-          ],
+          ),
         ),
       ),
     );
